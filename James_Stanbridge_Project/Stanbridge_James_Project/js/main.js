fe3f64f4eeab9707057f6ce6038b9219c5229279
jstanbridge <jstanbridge@fullsail.edu>
2014-09-19 18:38:21 -0400

    Week 3 Project Upload

--- James_Stanbridge_Project/Stanbridge_James_Project/js/main.js
+++ James_Stanbridge_Project/Stanbridge_James_Project/js/main.js
@@ -93,6 +93,190 @@ $(document).ready(function(){
 	
 	
 	
+	//Login Code 
+	$('#signinButton').click(function(){
+		var user = $('#user').val();
+		var pass = $('#pass').val();
+		console.log("This notifies you if the password is working");
+		$.ajax({
+			url: 'xhr/login.php',
+			type: 'post',
+			dataType: 'json',
+			data: {
+				username: user,
+				password: pass
+			},
+			success: function(response){
+				console.log("Test User");
+				if (response.error){
+					alert(response.error);
+				}else{
+					window.location.assign('dashboard.html')
+				};
+			}
+		});
+	});
+	
+	
+	
+	//Logout Code
+	$('#logOut').click(function(e){
+		e.preventDefault;
+		$.get('xhr/logout.php', function(){
+			window.location.assign('index.html')
+		})
+	});
+	
+	
+	
+	//Register Code
+	$('#register').on('click', function(){
+		var firstname= $('#first').val(),
+			lastname= $('#last').val();
+			username= $('#userName').val();
+			email= $('#email').val();
+			password= $('#password').val();
+			console.log(firstname+' '+lastname+' '+username+' '+email+' '+password);
+			
+		$.ajax({
+			url:'xhr/register.php',
+			type: 'post',
+			dataType: 'json',
+			data: {
+				firstname: firstname,
+				lastname: lastname,
+				username: username,
+				email: email,
+				password: password
+			},
+			success: function(response){
+				if (response.error){
+					alert(response.error);
+				}else{
+					window.location.assign('dashboard.html');
+				};
+			}
+		});
+	});
+	
+	
+	
+	//Signup Button Code
+	$('#signUp').click(function(){
+		window.location.assign('register.html')
+	});
+	
+	//Signup Button Code
+	$('#dboardbtn').click(function(){
+		window.location.assign('dashboard.html')
+	});
+	
+	
+	//Dynamic Username Display Code
+	$.getJSON("xhr/check_login.php", function(data){
+			console.log(data);
+			$.each(data, function(key, val){
+				console.log(val.user_n);
+				$(".userid").html("Welcome "+val.user_n+"!");
+			})
+	});
+	
+	
+	
+	//Go To Projects Page Code
+	$('.projbtn').on('click', function(e) {
+		e.preventDefault();
+		window.location.assign('projects.html');
+	});
+	
+	
+	
+	//Add New Project Code
+	$('#addButton').on('click', function() {
+
+		var projName = $('#pName').val(),
+		projDesc = $('#projectDescription').val(),
+		projDue = $('pDue').val(),
+		status = $('input[name = "status"]:checked').prop("id");
+	
+		$.ajax({
+			url: "xhr/new_project.php",
+			type: "post",
+			dataType: "json",
+			data: {
+				projectName: projName,
+				projectDescription: projDesc,
+				dueDate: projDue,
+				status: status
+			},
+			success: function(response) {
+				console.log('Testing for success');
+			
+				if(response.error) {
+					alert(response.error);
+				}else{
+					window.location.assign("projects.html");
+				};
+			}
+		});
+	});	
+	
 	
 
-});
+	//Get Projects Page Code
+	var projects = function(){
+	
+		$.ajax({
+			url: 'xhr/get_projects.php',
+			type: 'get',
+			dataType: 'json',
+			success: function(response){
+				if(response.error){
+					console.log(response.error);
+				}else{
+					
+					for(var i=0, j=response.projects.length; i < j; i++){
+						var result = response.projects[i];
+						
+						$(".projects").append(
+							'<div class="projectbox">' + 
+							" <input class='projectid' type='hidden' value='" + result.id + "'>" +
+							" Project Name: " + result.projectName + "<br>" +
+							" Project Description: " + result.projectDescription + "<br>" +
+							" Project Status: " + result.status + "<br>"
+							+ '<button class="editbtn">Edit</button>'
+							+ '<button class="deletebtn">Delete</button>'
+							+ '</div> <br>'
+						);
+					};
+					$('.deletebtn').on('click', function(e){
+						var pid = $(this).parent().find(".projectid").val();
+						console.log('test delete');
+						$.ajax({
+							url: 'xhr/delete_project.php',
+							data: {
+								projectID: pid
+							},
+							type: 'POST',
+							dataType: 'json',
+							success: function(response){
+								console.log('Testing for success');
+							
+								if(response.error) {
+									alert(response.error);
+								}else{
+									window.location.assign("projects.html");
+								};
+							}
+						});
+					});
+				}
+			}
+		})
+	}
+	projects();
+	
+	
+	
+
+});	//final closing tag
